1062197ebbe72f5df1d80b0d20dac3a5f705cec5
ruthypatooty <78903125+ruthypatooty@users.noreply.github.com>
2021-03-02 02:08:29 -0500

    Add files via upload

---
+++ Reimbursement_Layout/userpaneltest-pending.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>User Panel</title>
+
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="style.css?version=2">
+	
+</head>
+<body>
+<?php require("fixedfeulogo.inc");
+	 require("fixednavbar.inc")?>
+this is pending ticket page
+
+
+
+</body>
+</html>
